cd7d5b79a2bbe151af5a63715e158676144aded7
Syed Talha <syedt.in00@gmail.com>
2023-09-08 00:01:31 +0500

    all files uploaded to github

---
+++ Assests/js/app.js
@@ -0,0 +1,18 @@
+// ================= banner slider
+
+// var myIndex = 0;
+// carousel();
+
+// function carousel() {
+//   var x = document.querySelector(".carousel-content");
+//   myIndex++;
+
+//   if (myIndex > 3) {
+//     myIndex = 1;
+//   }
+
+//   x.style.transition = "transform 1s ease-in-out";
+//   x.style.transform = "translateX(-" + (myIndex - 1) * 33.33 + "%)";
+
+//   setTimeout(carousel, 2000); // Change image every 2 seconds
+// }
